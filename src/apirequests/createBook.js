55bc497c708e56a19f52da687ca055f3e4e00e55
che <che.blancoo58@gmail.com>
2021-12-05 14:06:07 +0100

    Delete comment from createBook.js

--- src/apirequests/createBook.js
+++ src/apirequests/createBook.js
@@ -2,7 +2,6 @@
 import data from '../helpers/data';
 
 const createNewBook = async (book, id) => {
-  console.log(book);
   const user = data();
   try {
     const rawResponse = await fetch(`http://127.0.0.1:3001/cats/${id}/books`, {
@@ -26,21 +25,4 @@ const createNewBook = async (book, id) => {
     return error;
   }
 };
-// const createNewBook = async (book, id) => {
-//   const user = data();
-//   try {
-//     const result = await axios.post('http://127.0.0.1:3001/cats/1/books', {
-//       name: book.name,
-//       author: book.author,
-//       isbn: book.isbn,
-//       number_of_pages: book.numberOfPages,
-//       cat_id: id,
-
-//     },
-//     { headers: { Authorization: `Bearer ${user.auth_token}` } });
-//     return result;
-//   } catch (err) {
-//     return err;
-//   }
-// };
 export default createNewBook;
