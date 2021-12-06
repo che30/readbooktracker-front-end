4bf8af8ca10ad596bb573e94e26e855376db293c
che <che.blancoo58@gmail.com>
2021-12-06 13:37:33 +0100

    Add measurement reducer

---
+++ src/reducers/Measurement.js
@@ -0,0 +1,17 @@
+import { PAGES_READ } from "../actions";
+
+const initialState = {
+  pagesRead: ''
+}
+const measurementReducer = (state = initialState, action) =>{
+  switch (action.type) {
+    case PAGES_READ :
+      return{
+        ...state,
+        pagesRead: action.pg
+      }
+    default:
+      return state
+  }
+}
+export default measurementReducer
