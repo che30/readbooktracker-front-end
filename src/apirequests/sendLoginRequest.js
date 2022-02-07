a5ec1e10fa1093bc2f5c5b839ed7b1ccdd407498
che <che.blancoo58@gmail.com>
2022-02-07 22:47:38 +0100

    Update sendLoginRequest

--- src/apirequests/sendLoginRequest.js
+++ src/apirequests/sendLoginRequest.js
@@ -9,9 +9,6 @@ const sendLoginRequest = async (email, password) => {
     });
     localStorage.setItem('auth_token',
       JSON.stringify(result.data.auth_token));
-    const cats = await axios.get('http://127.0.0.1:3001/cats',
-      { headers: { Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjozLCJleHAiOjE0ODg5MDEyNjR9.7txvLgDzFdX5NIUGYb3W45oNIXinwB_ITu3jdlG5Dds` } });
-    localStorage.setItem('categories', JSON.stringify(cats.data));
     return result;
   } catch (error) {
     return error;
