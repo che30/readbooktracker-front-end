4edec718ded1b470b42fef5beaef816b6e3cc11c
che <che.blancoo58@gmail.com>
2022-02-16 19:24:41 +0100

    Revert cor changes

--- src/apirequests/CreateUser.js
+++ src/apirequests/CreateUser.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const creatUser = async (userCredentials) => {
   const body = {
     username: userCredentials.username,
@@ -5,26 +7,11 @@ const creatUser = async (userCredentials) => {
     password: userCredentials.password,
     password_confirmation: userCredentials.passwordConfirmation,
   };
-  const url = 'https://mysterious-eyrie-66534.herokuapp.com/signup';
-  // try {
-  //   const result = await axios.post('https://mysterious-eyrie-66534.herokuapp.com/signup', body);
-  //   return result;
-  // } catch (error) {
-  //   return error;
-  // }
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'https://hidden-journey-97791.herokuapp.com/', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json',
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(body), // body data type must match "Content-Type" header
-  });
-  return response.json();
+  try {
+    const result = await axios.post('https://mysterious-eyrie-66534.herokuapp.com/signup', body);
+    return result;
+  } catch (error) {
+    return error;
+  }
 };
 export default creatUser;
