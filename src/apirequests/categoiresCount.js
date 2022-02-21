f40a54415389542cb0139093e9c4f9b5926d8c54
che <che.blancoo58@gmail.com>
2022-02-22 00:51:20 +0100

    comment categoriesCount.js

--- src/apirequests/categoiresCount.js
+++ src/apirequests/categoiresCount.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const queryAndCountCategories = async () => {
-  const data = JSON.parse(localStorage.getItem('data'));
-  try {
-    const result = await axios.get('https://hidden-journey-97791.herokuapp.com/cats',
-      { headers: { Authorization: `Bearer ${data.auth_token}` } });
-    return result;
-  } catch (error) {
-    return error;
-  }
-};
-export default queryAndCountCategories;
+// const queryAndCountCategories = async () => {
+//   const data = JSON.parse(localStorage.getItem('data'));
+//   try {
+//     const result = await axios.get('https://hidden-journey-97791.herokuapp.com/cats',
+//       { headers: { Authorization: `Bearer ${data.auth_token}` } });
+//     return result;
+//   } catch (error) {
+//     return error;
+//   }
+// };
+// export default queryAndCountCategories;
