8f8e003f45ced6a591c99bfc4908e8815060db65
che <che.blancoo58@gmail.com>
2022-02-23 06:58:59 +0100

    Update new api endpoint

--- src/apirequests/CreateNewMeasurementApi.js
+++ src/apirequests/CreateNewMeasurementApi.js
@@ -5,7 +5,7 @@ const newMeasurementApi = async (pgRead, bookId, date) => {
   const token = data();
   const decoded = jwtDecode(token);
   try {
-    const rawResponse = await fetch('https://read-book-api.herokuapp.com/measurments', {
+    const rawResponse = await fetch('https://read-book-api.herokuapp.com/api/measurments', {
       method: 'POST',
       headers: {
         Accept: 'application/json',
