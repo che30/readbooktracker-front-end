d99e52a48da4c70f6acee62cd9f1fde2f2f14a4c
che <che.blancoo58@gmail.com>
2022-03-02 23:04:03 +0100

    Update createNewMeasurement

--- src/helpers/backendLink.js
+++ src/helpers/backendLink.js
@@ -1,4 +1,4 @@
 // http://127.0.0.1:3001
 // https://read-book-api.herokuapp.com
-const backEndUrl = () => 'https://read-book-api.herokuapp.com';
+const backEndUrl = () => 'http://127.0.0.1:3001';
 export default backEndUrl;
